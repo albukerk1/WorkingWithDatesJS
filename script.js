517ecb0074a18015c8737d6296ed720efc289768
Matheus Albuquerque <matheusalbuquerque94@gmail.com>
2024-05-09 22:11:59 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+// Função que retorna o parâmetro Date maior, utilizando operador ternário (`? :`), que é uma forma condicional compacta para expressar a declaração if...else.
+function maiorData(date1, date2) {
+    return date1 > date2 ? date1 : date2;
+}
+
+// Calcula o intervalo entre duas datas, não importando a ordem de entrada
+function calcularIntervalo(date1, date2) {
+    if (date1 > date2) {
+        let temp = date1;
+        date1 = date2;
+        date2 = temp;
+    }
+
+    let diffMs = date2 - date1;
+    let diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+    return diffDays + " dias";
+}
+
+// Retorna a data atual no formato desejado
+function dataAtual() {
+    let now = new Date();
+    let dia = now.getDate();
+    let mes = now.getMonth() + 1; // Meses são indexados de 0 a 11
+    let ano = now.getFullYear();
+    let hora = now.getHours();
+    let minuto = now.getMinutes();
+
+    // Formata a data no padrão desejado
+    let dataFormatada = `${dia}/${mes}/${ano} ${hora}:${minuto}`;
+    return dataFormatada;
+}
+// Função para exibir Maior Data 
+function exibirMaiorData() {
+    let data1 = new Date(document.getElementById("data1").value);
+    let data2 = new Date(document.getElementById("data2").value);
+
+    // Ajusta as datas para considerar o fuso horário local
+    data1.setTime(data1.getTime() + data1.getTimezoneOffset() * 60 * 1000);
+    data2.setTime(data2.getTime() + data2.getTimezoneOffset() * 60 * 1000);
+
+    let resultado = maiorData(data1, data2);
+    let dia = resultado.getDate();
+    let mes = resultado.getMonth() + 1; // Meses são indexados de 0 a 11
+    let ano = resultado.getFullYear();
+    document.getElementById("resultado").innerText = "Maior data: " + dia + "/" + mes + "/" + ano;
+}
+
+function exibirIntervalo() {
+    let data1 = new Date(document.getElementById("data1").value);
+    let data2 = new Date(document.getElementById("data2").value);
+
+    // Ajusta as datas para considerar o fuso horário local
+    data1.setTime(data1.getTime() + data1.getTimezoneOffset() * 60 * 1000);
+    data2.setTime(data2.getTime() + data2.getTimezoneOffset() * 60 * 1000);
+
+    let resultado = calcularIntervalo(data1, data2);
+    document.getElementById("resultado").innerText = "Intervalo entre as datas: " + resultado;
+}
